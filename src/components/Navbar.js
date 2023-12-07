9ebe841a8f40db2a2dc8e0286faafd7f710682a9
FarihaNK <nawed.khan7904@gmail.com>
2023-12-07 00:35:59 -0500

    Add favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
-import HoursTable from "./HoursTable";
+import './Home.css';
+import AutoCareIcon from './Images/AutoCareIcon.jpeg';
 
 const Navbar = () => {
   return (
     <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#"><img src={AutoCareIcon} className="Icon" alt="Icon"/></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>

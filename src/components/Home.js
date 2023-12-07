926e68bf10d7cbe32d6fb57342cbd76dcab6ce57
FarihaNK <nawed.khan7904@gmail.com>
2023-12-06 21:36:50 -0500

    Add FAQ componenet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import './Home.css';
 import './HoursTable';
+import HomeCarousel from './HomeCarousel';
+import Services from './Services';
 import HoursTable from './HoursTable';
 import FAQ from './FAQ';
 
 const Home = () => {
   return (
     <div>
+      <HomeCarousel/>
       <div className='intro'>Give your car...WINGS!<br/>more info here</div>
       <HoursTable/>
+      <Services/>
       <FAQ/>
     </div>
   );

2ae8fc03f042daaa622e9e3ec0909110b4332e0a
FarihaNK <nawed.khan7904@gmail.com>
2023-12-20 01:34:52 -0500

    add service cards transition

--- src/components/Services.js
+++ src/components/Services.js
@@ -14,8 +14,23 @@ const Services = () => {
               <img src={Image_1} class="card-img-top" alt="..."/>
               <div class="card-body">
                 <h5 class="card-title">Express Lube Service</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.
-                This content is a little bit longer.</p>
+                <p className="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+                </p>
+                <p className="additional-text">
+                <strong>Additional Text:</strong> Lorem Ipsum is simply dummy
+                text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
               </div>
             </div>
           </div>
@@ -24,7 +39,19 @@ const Services = () => {
               <img src={Image_2} class="card-img-top" alt="..."/>
               <div class="card-body">
                 <h5 class="card-title">Brakes</h5>
-                <p class="card-text">This is a short card.</p>
+                <p class="card-text">This is a short card.
+                </p>
+                <p className="additional-text">
+                <strong>Additional Text:</strong> Lorem Ipsum is simply dummy
+                text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
             </div>
           </div>
@@ -33,7 +60,19 @@ const Services = () => {
               <img src={Image_3} class="card-img-top" alt="..."/>
               <div class="card-body">
                 <h5 class="card-title">Muffler</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.
+                </p>
+                <p className="additional-text">
+                <strong>Additional Text:</strong> Lorem Ipsum is simply dummy
+                text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
             </div>
           </div>
@@ -42,7 +81,19 @@ const Services = () => {
               <img src={Image_2} class="card-img-top" alt="..."/>
               <div class="card-body">
                 <h5 class="card-title">Engine</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.
+                </p>
+                <p className="additional-text">
+                <strong>Additional Text:</strong> Lorem Ipsum is simply dummy
+                text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
             </div>
           </div>
@@ -51,7 +102,19 @@ const Services = () => {
               <img src={Image_3} class="card-img-top" alt="..."/>
               <div class="card-body">
                 <h5 class="card-title">Transmission</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.
+                </p>
+                <p className="additional-text">
+                <strong>Additional Text:</strong> Lorem Ipsum is simply dummy
+                text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
             </div>
           </div>
@@ -59,8 +122,20 @@ const Services = () => {
             <div class="card h-100">
               <img src={Image_1} class="card-img-top" alt="..."/>
               <div class="card-body">
-                <h5 class="card-title">Filler</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                <h5 class="card-title">Oil Change</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.
+                </p>
+                <p className="additional-text">
+                <strong>Additional Text:</strong> Lorem Ipsum is simply dummy
+                text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
             </div>
           </div>
